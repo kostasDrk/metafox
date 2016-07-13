574d8fb2d718cb0148dce9ea200b67ea3dbbf217
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-07-13 21:32:54 +0300

    [Update scripts] To compile symbol table classes and clean symbol directory.

--- src/build.sh
+++ src/build.sh
@@ -6,13 +6,21 @@ java -jar lib/java-cup-11a.jar grammar.cup
 
 echo "[+] Compiling sym.java"
 javac sym.java
+
 echo "[+] Compiling parser.java"
 javac parser.java
+
 echo "[+] Compiling Lexer"
 javac MyLexer.java
+
 echo "[+] Compiling AST package"
 javac ast/*.java
+
+echo "[+] Compiling SymTable"
+javac symbol/*.java
+
 echo "[+] Compiling ASTVisitor"
 javac PrintASTVisitor.java
+
 echo "[+] Compilerception"
 javac MetafoxCompiler.java
--- src/clean.sh
+++ src/clean.sh
@@ -1,6 +1,13 @@
+echo "Clean-up directory."
 
 rm *.class
+
 rm ast/*class
+
 rm MyLexer.java*
+
 rm parser.java
+
 rm sym.java
+
+rm symbol/*.class
