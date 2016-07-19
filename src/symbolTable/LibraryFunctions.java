025a1f76502d3dbe5c807e05b05f353ee551cf36
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-07-19 03:43:27 +0300

    [symbolTable package] Add LibraryFunctions.java, which containts the enum with the supported library functions.

---
+++ src/symbolTable/LibraryFunctions.java
@@ -0,0 +1,29 @@
+package symbolTable;
+
+public enum LibraryFunctions {
+
+    PRINT("print"),
+    SQRT("sqrt"),
+    COS("cos"),
+    SIN("sin");
+
+    private final String _name;
+
+    private LibraryFunctions(String name) {
+        this._name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this._name;
+    }
+
+    public static boolean isLibraryFunction(String name) {
+        return name.equals(LibraryFunctions.PRINT.toString())
+                || name.equals(LibraryFunctions.SQRT.toString())
+                || name.equals(LibraryFunctions.COS.toString())
+                || name.equals(LibraryFunctions.SIN.toString());
+
+    }
+
+}
