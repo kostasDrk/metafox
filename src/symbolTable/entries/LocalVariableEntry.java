1af6b02abe58a719d5bae9c3c6f5c6809a8d0d8c
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-07-19 20:33:29 +0300

    [symbolTable] Add class to prepresent local variables.

---
+++ src/symbolTable/entries/LocalVariableEntry.java
@@ -0,0 +1,15 @@
+package symbolTable.entries;
+
+public class LocalVariableEntry extends AVariableEntry {
+
+    /**
+     * LocalVariableEntry Constructor
+     *
+     * @param name
+     * @param scope
+     */
+    public LocalVariableEntry(String name, int scope) {
+        super(name, scope);
+
+    }
+}
