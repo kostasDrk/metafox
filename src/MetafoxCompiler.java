69acf154caa156ec98cf1b84a5c5948cbbb759be
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-07-23 14:44:31 +0300

    [MetafoxCompiler] Call ExecutionASTVisitor.

--- src/MetafoxCompiler.java
+++ src/MetafoxCompiler.java
@@ -1,3 +1,4 @@
+
 import java.io.FileReader;
 import ast.ASTNode;
 import ast.ASTVisitor;
@@ -7,18 +8,20 @@ public class MetafoxCompiler {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Usage : Compiler <inputfile>");
-        }
-        else {
+        } else {
             try {
-                parser p = new parser( new MyLexer(new FileReader(args[0])) );
+                parser p = new parser(new MyLexer(new FileReader(args[0])));
 
                 ASTNode program = (ASTNode) p.parse().value;
-                
+
                 ASTVisitor printVisitor = new PrintASTVisitor();
                 program.accept(printVisitor);
                 System.out.println("\n\n**Parse ok**\n\n");
-            }
-            catch(Exception e) {
+
+                ASTVisitor executionASTVisitor = new ExecutionASTVisitor();
+                program.accept(executionASTVisitor);
+                System.out.println("\n\n**Execution ok**\n\n");
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
                 e.printStackTrace();
             }
