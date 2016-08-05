00d961bce8f3dc2723f688f2cfa4e8389723e7b8
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-08-05 23:39:25 +0300

    [Execution Environment: Add EnvironmentScope] Keeps the symbols for a specific scope.

---
+++ src/environment/EnvironmentScope.java
@@ -0,0 +1,39 @@
+package environment;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class EnvironmentScope {
+
+    private final HashMap<String, Value> _env;
+    private final int _scope;
+
+    EnvironmentScope(int scope) {
+        _env = new HashMap();
+        _scope = scope;
+    }
+
+    int getScope() {
+        return _scope;
+    }
+
+    Value lookup(String name) {
+        return _env.get(name);
+    }
+
+    void insert(String name, Value value) {
+        System.out.println(", scope " + _scope);
+        _env.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        String msg = "\n-------------------------- Scope: " + _scope + " --------------------------\n";
+
+        for (Map.Entry<String, Value> entry : _env.entrySet()) {
+            msg += String.format("%-20s %s", entry.getKey(), entry.getValue()) + "\n";
+        }
+        return msg;
+    }
+}
