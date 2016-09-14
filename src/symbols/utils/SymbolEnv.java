fe45e05cad4d4234053a46bfa5f603e414d7b41e
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-14 14:47:46 +0300

    [ASTReferences: SymbolEnv] Add symbolEnv class to encapsulate the enviromentscope in which an idetifier exists.

---
+++ src/symbols/utils/SymbolEnv.java
@@ -0,0 +1,18 @@
+package symbols.utils;
+
+import ast.IdentifierExpression;
+import environment.EnvironmentScope;
+
+public class SymbolEnv {
+
+    private final EnvironmentScope _env;
+
+    public SymbolEnv(EnvironmentScope env) {
+        _env = env;
+    }
+
+    public void setIdentifier(IdentifierExpression oldIdentifier, IdentifierExpression newIdentifier) {
+        _env.renameSymbol(new Symbol(oldIdentifier), new Symbol(newIdentifier));
+    }
+
+}
