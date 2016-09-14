4fd26a5db4c32937f3ac8cf8a35044fff431e901
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-14 14:51:45 +0300

    [ASTReferences: IdentifierExpression] Add symbolEnv field to keep the environment in which the symbol exists.

--- src/ast/IdentifierExpression.java
+++ src/ast/IdentifierExpression.java
@@ -1,26 +1,44 @@
 package ast;
 
+import symbols.utils.SymbolEnv;
 import symbols.value.Value;
 
 public class IdentifierExpression extends Lvalue {
 
-	private String _identifier;
+    private String _identifier;
+    private SymbolEnv _symbolEnv;
 
-	public IdentifierExpression(String identifier) {
-		this._identifier = identifier;
-	}
+    public IdentifierExpression(String identifier) {
+        _identifier = identifier;
+        _symbolEnv = null;
+    }
 
-	public String getIdentifier() {
-		return _identifier;
-	}
+    public String getIdentifier() {
+        return _identifier;
+    }
 
-	public void setIdentifier(String identifier) {
-		this._identifier = identifier;
-	}
+    public void setIdentifier(String identifier) {
+        IdentifierExpression oldIdentifier = new IdentifierExpression(_identifier);
+        _identifier = identifier;
 
-	@Override
-	public Value accept(ASTVisitor visitor) throws ASTVisitorException {
-		return visitor.visit(this);
-	}
-	
+        _symbolEnv.setIdentifier(oldIdentifier, this);
+    }
+
+    public SymbolEnv getSymbolEnv() {
+        return _symbolEnv;
+    }
+
+    public void setSymbolEnv(SymbolEnv symbolEnv) {
+        _symbolEnv = symbolEnv;
+    }
+
+    public void removeSymbolEnv() {
+        _symbolEnv = null;
+    }
+
+    @Override
+    public Value accept(ASTVisitor visitor) throws ASTVisitorException {
+        return visitor.visit(this);
+    }
+
 }
