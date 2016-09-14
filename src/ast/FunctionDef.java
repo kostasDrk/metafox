59f331a69a10c5bc1349d7a7443abd8086b60544
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-14 14:53:47 +0300

    [FunctionDef: Change name from String to IndentifierExpression] Add the methods to manage the changes.

--- src/ast/FunctionDef.java
+++ src/ast/FunctionDef.java
@@ -5,7 +5,7 @@ import symbols.value.Value;
 
 public class FunctionDef extends Statement {
 
-	private String _funcName;
+	private final IdentifierExpression _funcNameIdentifier;
 	private ArrayList<IdentifierExpression> _arguments;
 	private Block _body;
         private static int _anonumousFunctionCounter = 0 ;
@@ -16,33 +16,37 @@ public class FunctionDef extends Statement {
                     _anonumousFunctionCounter++;
                 }
                 
-                this._funcName = funcName;
-		this._arguments = arguments;
-		this._body = body;
+                _funcNameIdentifier = new IdentifierExpression(funcName);
+                _arguments = arguments;
+		_body = body;
 	}
-
+                
 	public String getFuncName(){
-		return this._funcName;
+		return _funcNameIdentifier.getIdentifier();
 	}
 
-	public void setFuncName(String funcName){
-		this._funcName = funcName;
+        public IdentifierExpression getFuncNameIdentifierExpr(){
+		return _funcNameIdentifier;
 	}
 
+        public void setFuncName(String funcName){
+		_funcNameIdentifier.setIdentifier(funcName);
+	}
+        
 	public ArrayList<IdentifierExpression> getArguments(){
-		return this._arguments;
+		return _arguments;
 	}
 
 	public void setArguments(ArrayList<IdentifierExpression> arguments){
-		this._arguments = arguments;
+		_arguments = arguments;
 	}
 
 	public Block getBody(){
-		return this._body;
+		return _body;
 	}
 
 	public void setBody(Block body){
-		this._body = body;
+		_body = body;
 	}
 
 	@Override
