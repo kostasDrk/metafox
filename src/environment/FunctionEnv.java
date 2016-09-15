ee58f4b9d286a3c2ee7d961ad4c9daea2fe6c865
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-16 02:39:22 +0300

    [LICENSE: Environment] Add license to all Environment classes and use ctrl + shfit + f.

--- src/environment/FunctionEnv.java
+++ src/environment/FunctionEnv.java
@@ -1,3 +1,29 @@
+/**
+ * Metafox - A DYNAMIC, INTERPRETED, META-PROGRAMMING LANGUAGE, RUN AND
+ * SUPPORTED BY ITS OWN INDEPENDENT INTERPRETER.
+ *
+ * UNIVERSITY OF CRETE (UOC)
+ *
+ * COMPUTER SCIENCE DEPARTMENT (UOC)
+ *
+ * https://www.csd.uoc.gr/
+ *
+ * CS-540 ADVANCED TOPICS IN PROGRAMMING LANGUAGES DEVELOPMENT
+ *
+ * LICENCE: This file is part of Metafox. Metafox is free: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * Metafox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Metafox. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016
+ *
+ */
 package environment;
 
 import symbols.value.StaticVal;
@@ -5,6 +31,15 @@ import symbols.value.Value;
 
 import static utils.Constants.FUNCTION_ENV_INIT_SCOPE;
 
+/**
+ * CLASS FunctionEnv
+ *
+ * @author Drakonakis Kostas  < kostasDrk  at csd.uoc.gr >
+ * @author Kokolaki Anna      < kokolaki   at csd.uoc.gr >
+ * @author Nikitakis Giorgos  < nikitak    at csd.uoc.gr >
+ *
+ * @version 1.0.0
+ */
 public class FunctionEnv extends Environment {
 
     private Value _returnVal;
