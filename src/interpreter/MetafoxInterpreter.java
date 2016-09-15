d130eb9852b92c56d0698a28fbcbf6fdb0de91bd
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-16 02:42:35 +0300

    [LICENSE: Interpreter package] Add license to all Interpreter package classes and use ctrl + shfit + f.

--- src/interpreter/MetafoxInterpreter.java
+++ src/interpreter/MetafoxInterpreter.java
@@ -1,3 +1,29 @@
+/**
+ * Metafox - A DYNAMIC, INTERPRETED, META-PROGRAMMING LANGUAGE, RUN AND
+ * SUPPORTED BY ITS OWN INDEPENDENT INTERPRETER.
+ *
+ * UNIVERSITY OF CRETE (UOC)
+ *
+ * COMPUTER SCIENCE DEPARTMENT (UOC)
+ *
+ * https://www.csd.uoc.gr/
+ *
+ * CS-540 ADVANCED TOPICS IN PROGRAMMING LANGUAGES DEVELOPMENT
+ *
+ * LICENCE: This file is part of Metafox. Metafox is free: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * Metafox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Metafox. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016
+ *
+ */
 package interpreter;
 
 import java.io.FileReader;
@@ -11,6 +37,15 @@ import ast.visitors.ToStringASTVisitor;
 import interpreter.parser.parser;
 import interpreter.lexer.lexer;
 
+/**
+ * CLASS MetafoxInterpreter
+ *
+ * @author Drakonakis Kostas  < kostasDrk  at csd.uoc.gr >
+ * @author Kokolaki Anna      < kokolaki   at csd.uoc.gr >
+ * @author Nikitakis Giorgos  < nikitak    at csd.uoc.gr >
+ *
+ * @version 1.0.0
+ */
 public class MetafoxInterpreter {
 
     public static void main(String[] args) {
@@ -26,8 +61,7 @@ public class MetafoxInterpreter {
                 //program.accept(printVisitor);
                 //System.out.println(printVisitor.toString());
                 //System.out.println("\n\n**Parse ok**\n\n");
-                
-                ASTVisitor executionASTVisitor = new ExecutionASTVisitor((Program)program);
+                ASTVisitor executionASTVisitor = new ExecutionASTVisitor((Program) program);
                 program.accept(executionASTVisitor);
                 // System.out.println("\n\n**Execution ok**\n\n");
             } catch (Exception e) {
--- src/interpreter/lexer/lexAnalysis.jflex
+++ src/interpreter/lexer/lexAnalysis.jflex
@@ -1,9 +1,45 @@
+/**
+ * Metafox - A DYNAMIC, INTERPRETED, META-PROGRAMMING LANGUAGE, RUN AND
+ * SUPPORTED BY ITS OWN INDEPENDENT INTERPRETER.
+ *
+ * UNIVERSITY OF CRETE (UOC)
+ *
+ * COMPUTER SCIENCE DEPARTMENT (UOC)
+ *
+ * https://www.csd.uoc.gr/
+ *
+ * CS-540 ADVANCED TOPICS IN PROGRAMMING LANGUAGES DEVELOPMENT
+ *
+ * LICENCE: This file is part of Metafox. Metafox is free: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * Metafox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Metafox. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016
+ *
+ */
 package interpreter.lexer;
 
 import static java.lang.System.out;
 import java.util.*;
 import java_cup.runtime.*;
 import interpreter.parser.sym;
+
+/**
+ * LEXER
+ *
+ * @author Drakonakis Kostas  < kostasDrk  at csd.uoc.gr >
+ * @author Kokolaki Anna      < kokolaki   at csd.uoc.gr >
+ * @author Nikitakis Giorgos  < nikitak    at csd.uoc.gr >
+ *
+ * @version 1.0.0
+ */
 %%
 
 %class lexer
