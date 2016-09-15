4ae76826164b0f9713144b9314c97f35cf7c9312
giorgosnikitakis <giorgosnikitakis@gmail.com>
2016-09-16 02:33:20 +0300

    [LICENSE: DataStructures] Add license to all DataStructures classes and use ctrl + shfit + f.

--- src/dataStructures/FoxArray.java
+++ src/dataStructures/FoxArray.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Metafox - A DYNAMIC, INTERPRETED, META-PROGRAMMING LANGUAGE, RUN AND
+ * SUPPORTED BY ITS OWN INDEPENDENT INTERPRETER.
+ *
+ * UNIVERSITY OF CRETE (UOC)
+ *
+ * COMPUTER SCIENCE DEPARTMENT (UOC)
+ *
+ * https://www.csd.uoc.gr/
+ *
+ * CS-540 ADVANCED TOPICS IN PROGRAMMING LANGUAGES DEVELOPMENT
+ *
+ * LICENCE: This file is part of Metafox. Metafox is free: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation (version 3 of the License).
+ *
+ * Metafox is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Metafox. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2016
+ *
  */
 package dataStructures;
 
 import java.util.HashMap;
 import java.util.Collection;
 
-import symbols.value.StaticVal;
-import symbols.value.Value;
-import symbols.value.Value_t;
-import symbols.value.DynamicVal;
+import symbols.value.*;
 
 import static utils.Constants.NULL;
 import static utils.Constants.UNDEFINED;
 
+/**
+ * CLASS FoxArray
+ *
+ * @author Drakonakis Kostas  < kostasDrk  at csd.uoc.gr >
+ * @author Kokolaki Anna      < kokolaki   at csd.uoc.gr >
+ * @author Nikitakis Giorgos  < nikitak    at csd.uoc.gr >
+ *
+ * @version 1.0.0
+ */
 public class FoxArray extends AFoxDataStructure {
 
     private final HashMap<Value, Value> _numberIndexedData;
